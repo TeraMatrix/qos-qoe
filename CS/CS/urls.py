6cb28d427ad1b1c4625d5a0b7d9ca3cb93f07259
johri21 <abhinavinsomniac@gmail.com>
2014-07-10 19:50:06 +0530

    Little changes : Added Comments

--- CS/CS/urls.py
+++ CS/CS/urls.py
@@ -5,9 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'CS.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^', include('Nocout.urls')),
     url(r'^',include('api.urls')),
 	url(r'^admin/', include(admin.site.urls))
